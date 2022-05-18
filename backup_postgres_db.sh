7267294937520bf01a4e6bfae586df58fc01acbe
Mateusz Plinta <mateusz.plinta@nokia.com>
2022-05-18 10:55:06 +0200

    update backupdb script

--- backup_postgres_db.sh
+++ backup_postgres_db.sh
@@ -2,10 +2,16 @@
 set -o pipefail
 timestamp=$(date +%s)
 db_backup_dir=/usr/local/share/postgres_db_backups
+container_name=postgres_db
 
 mkdir -p "$db_backup_dir"
 
 cd "$db_backup_dir"
+
+if [ "$( docker container inspect -f '{{.State.Running}}' $container_name )" != "true" ]; then
+    echo "Docker $container_name container is not running, so I am skipping performing db backup."
+    exit
+fi
 
 docker exec -i postgres_db /usr/bin/pg_dump -U postgres postgres | gzip -9 > $db_backup_dir/postgres-backup_$timestamp.sql.gz
 status=$?
@@ -13,12 +19,12 @@ status=$?
 file_no=$(find . -printf "%T@ %Tc %p\n" | grep './postgres' | sort -n | cut -f'9' -d' ' | wc -l)
 
 if [ "$file_no" -gt 3 ]; then
-  echo "More than 3 db.backups files. Will perform rolling update"
-  file_to_rm=$(find . -printf "%T@ %Tc %p\n" | grep './postgres' | sort -n | cut -f'9' -d' ' | head -n 1)
-  if [ "$status" -eq 0 ]; then
-    echo "Removing file $file_to_rm"
-    rm -f "$file_to_rm"
-  fi
+    echo "More than 3 db.backups files. Will perform rolling update"
+    file_to_rm=$(find . -printf "%T@ %Tc %p\n" | grep './postgres' | sort -n | cut -f'9' -d' ' | head -n 1)
+    if [ "$status" -eq 0 ]; then
+        echo "Removing file $file_to_rm"
+        rm -f "$file_to_rm"
+    fi
 fi
 
 
