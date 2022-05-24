86339e9f7af2c2f5a892f2886ce407d9a6826f5b
Plinta Mateusz <mateusz.plinta@nokia.com>
2022-05-24 16:09:03 +0200

    add test runs analyze and fix failmessagetypegroup serializer for regressionfilters

--- backend/tra/urls.py
+++ backend/tra/urls.py
@@ -33,13 +33,20 @@ urlpatterns = [
     #     - reg_filters, fb, test_instance, result, env_issue_type, analyzed_by, analyzed, testline_type,
     # All fields should be referenced by their primary keys
     path('test_runs/by_query/', views.TestRunsBasedOnQuery.as_view(), name='byquery'),
+
+    # test_runs/analyze_to_rp   
+    #       send POST with the following keys:
+    #            rp_ids             list of rp ids to analyze
+    #            comment            comment
+    #            result             result string
+    #            env_issue_type     env issue type string
     path('test_runs/analyze_to_rp/', views.TestRunsAnalyzeToRP.as_view(), name='analyze_to_rp'),
     path('test_runs/dist_fields_values/', views.TestRunsBasedOnQueryDictinctValues.as_view(), name='distinct_fields_values'),
-    # path('test_runs/by_reg_filter/', views.TestRunsBasedOnAllSubscribedRegressionFiltersView.as_view(), name='by_reg_filter'),
     path('test_runs/by_reg_filter/<int:rfid>/', views.TestRunsBasedOnRegressionFiltersView.as_view(), name='by_reg_filter_pk'),
 
-    # additional parameter limit=<int> can be provided to override default limit specified by regression filter
     path('test_runs/load_by_reg_filter/<int:rfid>/', views.LoadTestRunsToDBBasedOnRegressionFilter.as_view(), name='load_filtered_trs'),
+    # additional parameter limit=<int> can be provided to override default limit specified by regression filter
+
     path('test_runs/load_by_all_reg_filter/', views.LoadTestRunsToDBBasedOnAllRegressionFilters.as_view(), name='load_filtered_trs_by_all_rf'),
     path('test_runs/load_by_all_reg_filter_celery/', views.LoadTestRunsToDBBasedOnAllRegressionFiltersCelery.as_view(), name='load_filtered_trs_by_all_rf_celery'),
     path('', include(router.urls)),
