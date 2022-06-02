d39801a81a98cee15980e706e55f9277ffddfb10
jugacek <justyna.balcerzak@nokia.com>
2022-06-02 13:19:59 +0200

    Frontend: changed api address

--- frontend/src/services/test-results-analyzer/test-filters.service.js
+++ frontend/src/services/test-results-analyzer/test-filters.service.js
@@ -4,31 +4,31 @@ import authHeader from "../auth-header";
 export const getTestFilters = async (type) => {
     let url = "";
     if (type === 'all') {
-        url = 'api/regression_filters/';
+        url = 'api/tra/regression_filters/';
     } else if (type === 'owned') {
-        url = 'api/regression_filters/owned/';
+        url = 'api/tra/regression_filters/owned/';
     } else if (type === 'subscribed') {
-        url = 'api/regression_filters/subscribed/';
+        url = 'api/tra/regression_filters/subscribed/';
     }
     return (await axios.get(url, { headers: authHeader() }));
 }
 
 export const postTestFilter = async (testFilter) => {
-    return (await axios.post('api/regression_filters/', testFilter, { headers: authHeader() }));
+    return (await axios.post('api/tra/regression_filters/', testFilter, { headers: authHeader() }));
 }
 
 export const deleteTestFilter = async (id) => {
-    return (await axios.delete('api/regression_filters/' + id, { headers: authHeader() }));
+    return (await axios.delete('api/tra/regression_filters/' + id, { headers: authHeader() }));
 }
 
 export const getTestSets = async () => {
-    return (await axios.get('api/test_sets/', { headers: authHeader() }));
+    return (await axios.get('api/tra/test_sets/', { headers: authHeader() }));
 }
 
 export const postTestSet = async (testSet) => {
-    return (await axios.post('api/test_sets/', testSet, { headers: authHeader() }));
+    return (await axios.post('api/tra/test_sets/', testSet, { headers: authHeader() }));
 }
 
 export const getTestLineTypes = async () => {
-    return (await axios.get('api/testline_types/', { headers: authHeader() }));
+    return (await axios.get('api/tra/testline_types/', { headers: authHeader() }));
 }
--- frontend/src/services/test-results-analyzer/test-runs.service.js
+++ frontend/src/services/test-results-analyzer/test-runs.service.js
@@ -2,17 +2,17 @@ import axios from "axios";
 import authHeader from "../auth-header";
 
 export const getTestRuns = async (page) => {
-    return (await axios.get('api/test_runs/?page=' + page, { headers: authHeader() }));
+    return (await axios.get('api/tra/test_runs/?page=' + page, { headers: authHeader() }));
 }
 
 export const getTestRunsUsingFilter = async (filter, page, pageSize) => {
-    return (await axios.get('api/test_runs/by_query/?page=' + page + "&page_size=" + pageSize + "&" + filter, { headers: authHeader() }));
+    return (await axios.get('api/tra/test_runs/by_query/?page=' + page + "&page_size=" + pageSize + "&" + filter, { headers: authHeader() }));
 }
 
 export const getTestRunsFilters = async () => {
-    return (await axios.get('api/test_runs/dist_fields_values/', { headers: authHeader() }));
+    return (await axios.get('api/tra/test_runs/dist_fields_values/', { headers: authHeader() }));
 }
 
 export const postTestRun = async (analyzedObiect) => {
-    return (await axios.post('/api/test_runs/analyze_to_rp/', analyzedObiect, { headers: authHeader() }));
+    return (await axios.post('api/tra/test_runs/analyze_to_rp/', analyzedObiect, { headers: authHeader() }));
 }
