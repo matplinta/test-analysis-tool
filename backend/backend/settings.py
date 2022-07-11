8bb18ade0c5bf3b9535a43bbf7c87e4d1e5aa67f
Mateusz Plinta <mateusz.plinta@nokia.com>
2022-07-11 11:13:21 +0200

    remove default session auth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
     #     # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     #     # 'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
