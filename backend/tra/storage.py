5ad1f72e96725f4263b815006f307632f7a87181
Plinta Mateusz <mateusz.plinta@nokia.com>
2022-08-24 16:18:00 +0200

    update

--- backend/tra/storage.py
+++ backend/tra/storage.py
@@ -1,7 +1,11 @@
 from django.core.files.storage import Storage
 from django.conf import settings
+from django.utils import timezone
+from datetime import datetime
 import subprocess
 import os
+import shutil
+
 
 class UTECloudLogsStorage(Storage):
     def __init__(self, storage_local_path=None, storage_http_server=None):
@@ -14,9 +18,11 @@ class UTECloudLogsStorage(Storage):
         self._storage_http_server = storage_http_server
 
 
+    def path(self, name):
+        return os.path.join(self._storage_local_path, name)
+
     def _save(self, directory, url):
-        cwd = self._storage_local_path
-        path = os.path.join(cwd, directory)
+        path = self.path(directory)
         wget_cmd = f"wget -r -np -nH --cut-dirs=3 -R index.html -R index.html.tmp {url}"
         os.makedirs(path, exist_ok=True)
         proc = subprocess.Popen(wget_cmd, shell=True, stdout=subprocess.PIPE, cwd=path)
@@ -29,78 +35,88 @@ class UTECloudLogsStorage(Storage):
         return True if proc.returncode == 0 else False 
 
     
-    def save(self, name, content, max_length=None):
+    def save(self, name, url, max_length=None):
         name = self.get_available_name(name, max_length=max_length)
-        name = self._save(name, content)
-        # Ensure that the name returned from the storage system is still valid.
-        validate_file_name(name, allow_relative_path=True)
-        return name
+        is_saved = self._save(name, url)
+        return is_saved
 
 
     def delete(self, name):
         """
-        Delete the specified file from the storage system.
+        Delete the specified directory from the storage system.
         """
-        raise NotImplementedError(
-            "subclasses of Storage must provide a delete() method"
-        )
+        if not name:
+            raise ValueError("The name must be given to delete().")
+        name = self.path(name)
+        # If the file or directory exists, delete it from the filesystem.
+        try:
+            if os.path.isdir(name):
+                shutil.rmtree(name)
+            else:
+                os.remove(name)
+        except FileNotFoundError:
+            # FileNotFoundError is raised if the file or directory was removed
+            # concurrently.
+            pass
+        except OSError as e:
+            pass
+
 
     def exists(self, name):
         """
-        Return True if a file referenced by the given name already exists in the
-        storage system, or False if the name is available for a new file.
+        Return True if a directory referenced by the given name already exists in the
+        storage system, or False if the name is available for a new directory.
         """
-        raise NotImplementedError(
-            "subclasses of Storage must provide an exists() method"
-        )
+        return os.path.isdir(self.path(name))
+
 
     def listdir(self, path):
         """
         List the contents of the specified path. Return a 2-tuple of lists:
         the first item being directories, the second item being files.
         """
-        raise NotImplementedError(
-            "subclasses of Storage must provide a listdir() method"
-        )
+        current, dirs, files = next(os.walk(self.path(path)))
+        return dirs, files
+
 
     def size(self, name):
         """
         Return the total size, in bytes, of the file specified by name.
         """
-        raise NotImplementedError("subclasses of Storage must provide a size() method")
+        def get_dir_size(path):
+            total = 0
+            with os.scandir(path) as it:
+                for entry in it:
+                    if entry.is_file():
+                        total += entry.stat().st_size
+                    elif entry.is_dir():
+                        total += get_dir_size(entry.path)
+            return total
+
+        return get_dir_size(self.path(name))
+
 
     def url(self, name):
         """
         Return an absolute URL where the file's contents can be accessed
         directly by a web browser.
         """
-        raise NotImplementedError("subclasses of Storage must provide a url() method")
+        return f"{self._storage_http_server}/{name}"
+
+    def _datetime_from_timestamp(self, ts):
+        """
+        If timezone support is enabled, make an aware datetime object in UTC;
+        otherwise make a naive one in the local timezone.
+        """
+        tz = timezone.utc if settings.USE_TZ else None
+        return datetime.fromtimestamp(ts, tz=tz)
 
     def get_accessed_time(self, name):
-        """
-        Return the last accessed time (as a datetime) of the file specified by
-        name. The datetime will be timezone-aware if USE_TZ=True.
-        """
-        raise NotImplementedError(
-            "subclasses of Storage must provide a get_accessed_time() method"
-        )
+        return self._datetime_from_timestamp(os.path.getatime(self.path(name)))
 
     def get_created_time(self, name):
-        """
-        Return the creation time (as a datetime) of the file specified by name.
-        The datetime will be timezone-aware if USE_TZ=True.
-        """
-        raise NotImplementedError(
-            "subclasses of Storage must provide a get_created_time() method"
-        )
+        return self._datetime_from_timestamp(os.path.getctime(self.path(name)))
 
     def get_modified_time(self, name):
-        """
-        Return the last modified time (as a datetime) of the file specified by
-        name. The datetime will be timezone-aware if USE_TZ=True.
-        """
-        raise NotImplementedError(
-            "subclasses of Storage must provide a get_modified_time() method"
-        )
-
+        return self._datetime_from_timestamp(os.path.getmtime(self.path(name)))
     
